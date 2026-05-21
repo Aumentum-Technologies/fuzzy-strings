5ae794a9b7ad63f0c056abbd86b7c842e0f0fce7
Sean Lamberson <sean.lamberson@aumentumtech.com>
2026-05-21 15:13:47 -0400

    Add tools and update nuspec

---
+++ Tools/install.ps1
@@ -0,0 +1,11 @@
+param($installPath, $toolsPath, $package, $project)
+
+$solutionDir = Split-Path -Path $project.DTE.Solution.FullName -Parent 
+
+Write-Host "SolutionDir: " $solutionDir
+
+& ($solutionDir + "\BuildTools\InstallDllPackage.ps1") -InstallPath $installPath -ToolsPath $toolsPath -Package $package -Project $project
+
+if (-not $?) {
+  throw "Package installation failed";
+}
